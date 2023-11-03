793633181860563c2bfaa8ada200de56bca340f2
Artur Amaral <artur.amaral@mettainnovations.com.br>
2023-11-03 09:58:40 -0300

    Alocação dinâmica funcionando.

--- trab03/kruskal.c
+++ trab03/kruskal.c
@@ -5,12 +5,76 @@ typedef struct {
     int u, v, weight;
 } Edge;
 
+/*Input já está ordenado como premissa, implementei a toa*/
+/*int sortByWeight(Edge* elist, int nInputs)
+{
+    int i, j;
+    Edge aux;
+
+    for (i = 1; i < nInputs; i++) 
+    {
+        for (j = 0; j < nInputs - 1; j++) 
+        {
+             if (elist[j].weight > elist[j + 1].weight)
+             {
+                aux          = elist[j];
+                elist[j]     = elist[j + 1];
+                elist[j + 1] = aux;
+             }
+        }
+    }
+
+    return 0;
+}*/
+
+int cycleFilter(Edge *mstCandidate)
+{
+
+
+
+    return 0;
+}
+
+/* Recebe a lista de grafos do grafo e retorna o peso da "Minimum spanning tree". */
+int kruskalMstWeight(Edge *elist, int numEdges)
+{
+    int weight = 0;
+    Edge e;
+    Edge* mstCandidate = NULL;
+    Edge* mst = NULL;
+
+    /* Iteração principal. Monta um candidato à MST aresta a aresta, verificando, a cada etapa, se criam ciclos */
+    int i;
+    int nEdgesOnMst = 0;
+    int rc = 0;
+    for (i = 0; i < numEdges; i++)
+    {
+        nEdgesOnMst++;
+
+        e = elist[i];
+        
+        mstCandidate = (Edge*) realloc(mstCandidate, nEdgesOnMst * sizeof(Edge) );
+        
+        mstCandidate[i] = e;
+        
+        printf("%d %d %d\n", mstCandidate[i].u, mstCandidate[i].v, mstCandidate[i].weight);
+    }
+
+    for (i = 0; i < numEdges; i++)
+        weight += mstCandidate[i].weight;
+
+    free(mstCandidate);
+
+    return weight;
+}
+
 int main() 
 {
 
     int numVertices, numEdges;
     int *edges = NULL;
     int nInputs = 0;
+    int MstWeight;
 
 /*  Array final.  
     int edge[7][3] = { { 1, 2, 3 }, 
@@ -30,13 +94,15 @@ int main()
     {
         nInputs++;
         elist = (Edge*) realloc(elist, nInputs * sizeof(Edge) );
+        /*Printf aresta*/
         elist[nInputs-1] = e;
     }
 
-    int i = 0;
-    for (i = 0; i < nInputs; i++)
-        printf("%d - %d: %d\n", elist[i].u, elist[i].v, elist[i].weight);
+    MstWeight = kruskalMstWeight(elist, nInputs);
 
+    printf("Minimum spanning forest weight: %d\n", MstWeight);
+
+    free(elist);
 
     return 0; 
 }
